7e97ce6571b6f3f2e5984a5597005311f25dd40b
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 01:02:30 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -50,8 +50,8 @@ end
 # :temperature_farenheit => 66.3
 # })
 
-temp = 66.3 
-temp - 32 = new_temp
-new_temp *5 = almost_temp
-almost_temp/9 = celsius
+temp == 66.3 
+temp - 32 == new_temp
+new_temp *5 == almost_temp
+almost_temp/9 == celsius
 puts celsius 
